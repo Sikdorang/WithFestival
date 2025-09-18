bceee9e56e93e7bd0d0a3d74021e018742f4bdee
shl0501 <shl050105010501@gmail.com>
2025-09-18 15:11:06 +0900

    feat(be): 소켓

--- apps/backend/src/waiting/waiting.module.ts
+++ apps/backend/src/waiting/waiting.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+
+import { WebSocketModule } from '../websocket/websocket.module';
+
 import { WaitingController } from './waiting.controller';
 import { WaitingService } from './waiting.service';
 
 @Module({
+  imports: [WebSocketModule],
   controllers: [WaitingController],
   providers: [WaitingService],
   exports: [WaitingService],
--- apps/backend/src/websocket/websocket.module.ts
+++ apps/backend/src/websocket/websocket.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 
+import { OrderGateway } from './order.gateway';
 import { AppWebSocketGateway } from './websocket.gateway';
 import { WebSocketService } from './websocket.service';
 
 @Module({
-  providers: [AppWebSocketGateway, WebSocketService],
-  exports: [WebSocketService],
+  providers: [AppWebSocketGateway, OrderGateway, WebSocketService],
+  exports: [WebSocketService, OrderGateway],
 })
 export class WebSocketModule {}
